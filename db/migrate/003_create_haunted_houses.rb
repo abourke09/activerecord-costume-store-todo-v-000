7ddeec6cb820f1310da341b82713f3639de618b4
Annie Bourke <aabourke@gmail.com>
2018-05-10 20:57:01 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,6 +9,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.boolean :family_friendly
       t.string :opening_date
       t.string :closing_date
+      t.string :description
+      
     end
   end
 end
