885c9614c66a80bf91eef6c92a744a7f7e56e820
Annie Bourke <aabourke@gmail.com>
2018-05-10 21:18:19 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,8 +7,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.date :opening_time
-      t.date :closing_time
+      t.text :opening_time
+      t.text :closing_time
     end
   end
 end
