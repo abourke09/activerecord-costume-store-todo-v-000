f0733ac1f664d4fbc35145c84735c33914beed68
Annie Bourke <aabourke@gmail.com>
2018-05-10 21:00:39 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,6 +15,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
       t.string :image_url
       t.string :size
       t.datetime :created_at
+      t.datetime :updated_at
     end
   end
 end
